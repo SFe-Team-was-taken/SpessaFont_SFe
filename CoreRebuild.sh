75936c07332745a52b27fac567606b49c778b443
sylvia-leaf <33123585+sylvia-leaf@users.noreply.github.com>
2025-09-03 03:44:23 +0100

    Force rebase again

--- CoreRebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf ./node_modules
-npm install
-npm uninstall spessasynth_core spessasynth_lib
-npm install ../spessasynth_core ../spessasynth_lib
-npm run debug
-npm run build
-npm run dev
